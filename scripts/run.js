3cc6d5354812f544c2cf4f0452ae9f3ab4480605
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-05-20 00:39:37 +0530

    add parameters for eren, mikasa, armin

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,12 @@
 const main = async () => {
     const gameContractFactory = await ethers.getContractFactory("MyEpicGame");
 
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Eren", "Mikasa", "Armin"],
+        ["https://imgur.com/lSPLbjE.png", "https://i.imgur.com/5elwz6i.png", "https://i.imgur.com/ZTgfFAL.png"],
+        [1000, 500, 250],
+        [200,150,100]
+    );
 
     await gameContract.deployed();
     console.log("Contrtact deployed at: ", gameContract.address);
